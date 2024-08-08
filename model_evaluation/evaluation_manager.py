ed243a0daf638e1453cafdd5a7de08b8e2519202
Alex <alexandru.solomon9999@gmail.com>
2024-08-08 17:40:31 +0300

    Updated the evaluation module

--- model_evaluation/evaluation_manager.py
+++ model_evaluation/evaluation_manager.py
@@ -15,8 +15,8 @@ device = torch.device('cuda')
 # constant paths
 BASE = r"E:\Alex\UniBuc\MasterThesis\src"
 DATA_PATH = os.path.join(BASE, "data")
-MODEL_DIR_PATH = os.path.join(BASE, "recorded_models", "policy_methods_models", "policy_model_10_dir")
-MODEL_FILE_PATH = os.path.join(MODEL_DIR_PATH, "policy_model_10")
+MODEL_DIR_PATH = os.path.join(BASE, "recorded_models", "policy_methods_models", "policy_model_13_dir")
+MODEL_FILE_PATH = os.path.join(MODEL_DIR_PATH, "policy_model_13")
 sat_data_config = os.path.join(DATA_PATH, "default_sat_data_config.json")
 
 evaluator = policy_evaluator.PolicyEvaluator(
@@ -25,3 +25,7 @@ evaluator = policy_evaluator.PolicyEvaluator(
     model_file_path=MODEL_FILE_PATH,
     model_evaluation_path=MODEL_DIR_PATH,
     sat_data_config=sat_data_config)
+
+evaluator.perform_evaluation(game_env=evaluator.game_env,
+                             policy=evaluator.policy,
+                             num_runs=3)
--- model_evaluation/policy_evaluator.py
+++ model_evaluation/policy_evaluator.py
@@ -6,6 +6,7 @@ import numpy as np
 import datetime
 import torch
 import json
+import shutil
 
 import utils
 import models
@@ -43,7 +44,7 @@ class PolicyEvaluator:
 
         # setting up the environment
         env = gym.make('gym_satellite_ca:gym_satellite_ca/CollisionAvoidance-v0',
-                                 satellite=init_sat)
+                       satellite=init_sat)
 
         # set up the observation processing class
         tca_time_lapse_max_abs_val = env.observation_space['tca_time_lapse'].high[0]
@@ -81,12 +82,13 @@ class PolicyEvaluator:
 
         return policy, optimizer, checkpoint_epoch, checkpoint_loss
 
-    def play_game_once(self, game_env, policy: torch.nn.Module, policy_utils):
+    def play_game_once(self, game_env, policy: torch.nn.Module):
         # setting the policy to the evaluation mode
         policy.eval()
 
         # resetting the game and getting the firs obs
         obs, _ = game_env.reset()
+        final_info = None
 
         # condition for the game to be over
         done = False
@@ -109,16 +111,73 @@ class PolicyEvaluator:
             action = action_normal_distribution.sample().to(device=self.device)
 
             obs, reward, done, truncated, info = game_env.step(action.tolist())
+            if done:
+                final_info = info
 
             raw_rewards = torch.cat((raw_rewards, torch.tensor([reward]).to(device=self.device)))
 
-        # get the list of scores for each action
-        scores_per_action = policy_utils.compute_discount_rate_reward(list_of_rewards=raw_rewards)
+        return raw_rewards, final_info
 
-        return scores_per_action, raw_rewards
+    def perform_evaluation(self, game_env, policy, num_runs=10):
+        # instantiate the dictionary containing the general status of the execution
+        goals_overall_status_dict = {
+            "num_runs": num_runs,
+            "collision_avoided": 0,
+            "returned_to_init_orbit": 0,
+            "drifted_out_of_bounds": 0,
+            "fuel_used_perc": 0.0,
+            "raw_rewards_sum": 0.0,
+            "individual_run_goals": {}
+        }
 
-    def perform_evaluation(self):
-        loss, scores_per_action, raw_rewards = self.play_game_once(game_env=game_env,
-                                                                   policy=policy,
-                                                                   train=False,
-                                                                   optimizer=optimizer)
+        # create the plots directory
+        self.create_plots_directory()
+
+        for num_idx in range(num_runs):
+            raw_rewards, final_info = self.play_game_once(game_env=game_env,
+                                                          policy=policy)
+            current_goals_status = {
+                "collision_avoided": final_info["collision_avoided"],
+                "returned_to_init_orbit": final_info["returned_to_init_orbit"],
+                "drifted_out_of_bounds": final_info["drifted_out_of_bounds"],
+                "fuel_used_perc": final_info["fuel_used_perc"],
+                "raw_rewards": sum(raw_rewards.cpu().numpy().tolist())
+            }
+            goals_overall_status_dict["individual_run_goals"].update(current_goals_status)
+            goals_overall_status_dict = self.updated_goals_dict(goals_overall_status_dict,
+                                                                current_goals_status)
+
+        utils.save_json(dict_=goals_overall_status_dict,
+                        json_path=os.path.join(self.model_evaluation_path, "goals_evaluation.json"))
+
+    def create_plots_directory(self):
+        plots_dir_path = os.path.join(self.model_evaluation_path, "evaluation_plots")
+
+        # if the directory does not exist, create it, otherwise delete its content
+        if not os.path.isdir(plots_dir_path):
+            os.mkdir(plots_dir_path)
+        else:
+            for filename in os.listdir(plots_dir_path):
+                file_path = os.path.join(plots_dir_path, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    @staticmethod
+    def updated_goals_dict(goals_overall_status_dict, current_goals_status):
+        if current_goals_status["collision_avoided"] is True:
+            goals_overall_status_dict["collision_avoided"] += 1
+        if current_goals_status["returned_to_init_orbit"] is True:
+            goals_overall_status_dict["returned_to_init_orbit"] += 1
+        if current_goals_status["drifted_out_of_bounds"] is True:
+            goals_overall_status_dict["drifted_out_of_bounds"] += 1
+        goals_overall_status_dict["fuel_used_perc"] += (current_goals_status["fuel_used_perc"] /
+                                                        goals_overall_status_dict["num_runs"])
+        goals_overall_status_dict["raw_rewards_sum"] += (current_goals_status["raw_rewards"] /
+                                                         goals_overall_status_dict["num_runs"])
+
+        return goals_overall_status_dict
