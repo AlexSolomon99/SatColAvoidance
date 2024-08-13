3ff1ce0e539e420a6a8a531a8f09ddca1ac1145c
Alex <alexandru.solomon9999@gmail.com>
2024-08-13 12:41:07 +0300

    Created the dqn evaluator

---
+++ model_evaluation/evaluation_manager_dqn.py
@@ -0,0 +1,35 @@
+import os
+import torch
+import datetime
+
+import dqn_evaluator
+
+import sys
+
+sys.path.append(r'E:\Alex\UniBuc\MasterThesis\gym-satellite-ca')
+
+from gym_satellite_ca.envs import satDataClass
+
+# set device
+device = torch.device('cuda')
+
+# constant paths
+BASE = r"E:\Alex\UniBuc\MasterThesis\src"
+DATA_PATH = os.path.join(BASE, "data")
+MODEL_DIR_PATH = os.path.join(BASE, "recorded_models", "dqn_models", "dqn_model_4_dir")
+MODEL_FILE_PATH = os.path.join(MODEL_DIR_PATH, "dqn_model_4")
+sat_data_config = os.path.join(DATA_PATH, "default_sat_data_config.json")
+
+print(f"{datetime.datetime.now()} - Evaluation started!")
+evaluator = dqn_evaluator.DQNEvaluator(
+    device=device,
+    model_dir_path=MODEL_DIR_PATH,
+    model_file_path=MODEL_FILE_PATH,
+    model_evaluation_path=MODEL_DIR_PATH,
+    sat_data_config=sat_data_config)
+
+evaluator.perform_evaluation(game_env=evaluator.game_env,
+                             policy=evaluator.policy,
+                             num_runs=100)
+
+print(f"{datetime.datetime.now()} - Evaluation finished!")
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from models.policy_methods_nn import SatColAvoidPolicy
+from models.dqn_nn import QNetwork
