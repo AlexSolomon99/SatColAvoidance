93da2331377c2139894fb2fac21cdfdf46c42f59
Alex <alexandru.solomon9999@gmail.com>
2024-08-15 13:28:48 +0300

    Updated the ppo algorithm to work for the cartpole env

--- ppo_alg/mpi_pytorch.py
+++ ppo_alg/mpi_pytorch.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import torch
 from mpi4py import MPI
-from ppo.mpi_tools import broadcast, mpi_avg, num_procs, proc_id
+from ppo_alg.mpi_tools import broadcast, mpi_avg, num_procs, proc_id
 
 def setup_pytorch_for_mpi():
     """
