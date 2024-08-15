e2f788ab0ac43bc4c545e80714ee00126908c926
Alex <alexandru.solomon9999@gmail.com>
2024-08-16 00:01:59 +0300

    Updated the ppo algorithm

--- ppo_alg/ppo_manager.py
+++ ppo_alg/ppo_manager.py
@@ -7,30 +7,52 @@ from ppo_alg.mpi_tools import mpi_fork
 import utils
 import ppo_alg.core as core
 
-# constant paths
-BASE = r"E:\Alex\UniBuc\MasterThesis\src"
-DATA_PATH = os.path.join(BASE, "data")
-
-# environment setup
-# sat_data_config = os.path.join(DATA_PATH, "default_sat_data_config.json")
-# env, data_preprocessing = utils.set_up_environment(sat_data_config=sat_data_config)
-env = gym.make("CartPole-v1", max_episode_steps=100)
-
 # ppo_alg algorithm parameters
 num_cpu = 1
 seed = 0
-steps_per_epoch = 4000
+# 1 play goes for 1151 steps, so we choose 5 games per epoch
+steps_per_epoch = 5 * 1151
 epochs = 10
 gamma = 0.99
+MODEL_NAME = "ppo_model"
+obs_dim = 56
+
+# constant paths
+BASE = r"E:\Alex\UniBuc\MasterThesis\src"
+DATA_PATH = os.path.join(BASE, "data")
+RECORDED_MODELS_PATH = os.path.join(BASE, "recorded_models")
+PPO_METHODS_MODELS = os.path.join(RECORDED_MODELS_PATH, "ppo_models")
+
+if not os.path.isdir(RECORDED_MODELS_PATH):
+    os.mkdir(RECORDED_MODELS_PATH)
+
+if not os.path.isdir(PPO_METHODS_MODELS):
+    os.mkdir(PPO_METHODS_MODELS)
+
+# paths - set up the model record dictionary
+ppo_record_dict_path = os.path.join(PPO_METHODS_MODELS, "dqn_record.json")
+
+if not os.path.isfile(ppo_record_dict_path):
+    model_record_dict = {}
+    model_record_last_idx = 0
+else:
+    model_record_dict = utils.read_json(json_path=ppo_record_dict_path)
+    model_record_last_idx = len(model_record_dict.keys())
+experiment_name = f"{MODEL_NAME}_{model_record_last_idx + 1}"
+
+# environment setup
+sat_data_config = os.path.join(DATA_PATH, "default_sat_data_config.json")
+env, data_preprocessing = utils.set_up_environment(sat_data_config=sat_data_config)
 
 # set up the ppo_alg algorithm
 mpi_fork(num_cpu)  # run parallel code with mpi
 
-logger_kwargs = setup_logger_kwargs(exp_name="ppo_try_cartpole",
+logger_kwargs = setup_logger_kwargs(exp_name=experiment_name,
                                     seed=seed,
-                                    data_dir=DATA_PATH)
+                                    data_dir=PPO_METHODS_MODELS)
 
 # run the ppo_alg algorithm
-ppo(env=env, actor_critic=core.MLPActorCritic, gamma=gamma,
+ppo(env=env, data_preprocessing=data_preprocessing, obs_dim=obs_dim, actor_critic=core.MLPActorCritic, gamma=gamma,
     seed=seed, steps_per_epoch=steps_per_epoch, epochs=epochs,
-    logger_kwargs=logger_kwargs)
+    logger_kwargs=logger_kwargs, save_freq=1, model_record_dict=model_record_dict,
+    model_record_last_idx=model_record_last_idx, record_dict_path=ppo_record_dict_path)
