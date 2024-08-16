c8997af67f83c7fd5ac0e8e0921f8252fe4095da
Alex <alexandru.solomon9999@gmail.com>
2024-08-16 17:34:59 +0300

    Updated the ppo algorithm and its evaluator

---
+++ model_evaluation/evaluation_manager_ppo.py
@@ -0,0 +1,35 @@
+import os
+import torch
+import datetime
+
+import ppo_evaluator
+
+import sys
+
+sys.path.append(r'E:\Alex\UniBuc\MasterThesis\gym-satellite-ca')
+
+from gym_satellite_ca.envs import satDataClass
+
+# set device
+device = torch.device('cpu')
+
+# constant paths
+BASE = r"E:\Alex\UniBuc\MasterThesis\src"
+DATA_PATH = os.path.join(BASE, "data")
+MODEL_DIR_PATH = os.path.join(BASE, "recorded_models", "ppo_models", "ppo_model_1", "ppo_model_1_s0")
+MODEL_FILE_PATH = os.path.join(MODEL_DIR_PATH, "ppo_model")
+sat_data_config = os.path.join(DATA_PATH, "default_sat_data_config.json")
+
+print(f"{datetime.datetime.now()} - Evaluation started!")
+evaluator = ppo_evaluator.PPOEvaluator(
+    device=device,
+    model_dir_path=MODEL_DIR_PATH,
+    model_file_path=MODEL_FILE_PATH,
+    model_evaluation_path=MODEL_DIR_PATH,
+    sat_data_config=sat_data_config)
+
+evaluator.perform_evaluation(game_env=evaluator.game_env,
+                             policy=evaluator.policy,
+                             num_runs=10)
+
+print(f"{datetime.datetime.now()} - Evaluation finished!")
